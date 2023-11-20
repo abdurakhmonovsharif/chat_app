5f2e6a9d40d2969209e1b593f5463b6da7244cd7
abdurakhmonovsharif <fullstackdeveloper4413@gmail.com>
2023-11-20 23:58:28 +0500

    first commit

--- src/main/java/com/example/chat_app/Entity/User.java
+++ src/main/java/com/example/chat_app/Entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@RequiredArgsConstructor
 @Table(name = "Users")
 public class User {
     @Id
